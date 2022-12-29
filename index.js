9d989ca691573e0b16ae3160463f54fc74927338
Ben Moreno <Bemor8789@gmail.com>
2022-12-28 22:12:58 -0800

    JavaScript

---
+++ index.js
@@ -0,0 +1,20 @@
+function greet(name, height, country) {
+    console.log(name);
+  }
+
+  greet("Ben");
+  alert("Ben");
+
+  function greet2(height) {
+    console.log(height);
+  }
+
+  greet("5'9");
+  alert("5'9");
+
+  function greet3(country) {
+    console.log(country);
+  }
+
+  greet("USA");
+  alert("USA");
