27e78a84410512881de7a48b7bf2e899408dd6e6
Ben Moreno <Bemor8789@gmail.com>
2023-01-02 18:57:11 -0800

    fix errors

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 function greet(name, height, country) {
-    console.log(name);
-  }
-
-  greet("Ben");
-  alert("Ben");
-
-  function greet2(height) {
-    console.log(height);
-  }
-
-  greet("5'9");
-  alert("5'9");
-
-  function greet3(country) {
-    console.log(country);
-  }
-
-  greet("USA");
-  alert("USA");
+  alert(name + height + country);
+}
+greet("Ben\n", "5'9\n", "USA\n");
